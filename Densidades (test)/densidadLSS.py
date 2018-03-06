5e762fdea4b2e537a4c65060ab86640b7da0113b
PaipaPsyche <paipamurderer@gmail.com>
2018-03-05 23:37:14 -0500

    Stablishing grid

--- Densidades (test)/densidadLSS.py
+++ Densidades (test)/densidadLSS.py
@@ -40,17 +40,31 @@ def segregar_corte_z(z,dz):
 			Vzseg.append(dataVz[i])
 			Mseg.append(dataM[i])
 #============================
-segregar_corte_z(600,10)
+segregar_corte_z(200,10)
 #============================
 #graph
 fig=plt.figure(figsize=(10,10))
 ax=Axes3D(fig)
 ax.set_zlim(Lzi,Lzs)
-ax.scatter(Xseg,Yseg,Zseg,s=0.3)
+ax.scatter(Xseg,Yseg,Zseg,c='b',s=0.3,depthshade=False)
+segregar_corte_z(1000,10)
+ax.scatter(Xseg,Yseg,Zseg,c='g',s=0.3,depthshade=False)
+#Graphing 2 slides
+ax.set_xlabel("eje X")
+ax.set_ylabel("eje Y")
+ax.set_zlabel("eje Z")
+fig.savefig('secton3D.png')
 #============================
 #graph of the cut
 figP=plt.figure(figsize=(10,10))
-plt.scatter(Xseg,Yseg,s=0.3)
+ax2D = figP.gca()
+ax2D.set_xticks(np.arange(0, 1260, 60))
+ax2D.set_yticks(np.arange(0, 1260,60))
+plt.scatter(Xseg,Yseg,s=0.4)
+plt.xlabel("eje X")
+plt.ylabel("eje Y")
+plt.grid()
+figP.savefig('secton2D.png')
 #============================
 #NEXT: sectionate the 2D cut into pixels with density data and CM velocity.
 #============================
