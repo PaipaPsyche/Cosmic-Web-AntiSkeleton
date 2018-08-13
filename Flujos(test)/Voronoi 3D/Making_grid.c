ba449bdc9f48c743670bfb86cccb6051905372a7
PaipaPsyche <paipamurderer@gmail.com>
2018-08-13 07:25:34 -0500

    voronoi - C import

---
+++ Flujos(test)/Voronoi 3D/Making_grid.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+//CONSTANTES Y VARIABLES==========================
+int n_side=120;
+
+int Lbox= 600;
+int l_side= Lbox/n_side;
+
+double Vx_grid[n_side][n_side][n_side]
+double Vy_grid[n_side][n_side][n_side]
+double vz_grid[n_side][n_side][n_side]
+
+FILE*sampleIN;
+FILE*sampleOUT;
+
+double x*;
+double y*;
+double z*;
+
+
+
+
+
+
+
+
+//FUNCIONES==========================
+
+
+
+
+
+
+
+
+
+
+
+//MAIN==========================
+int main()
+{
+
+
+
+
+
+return 0;
+}
